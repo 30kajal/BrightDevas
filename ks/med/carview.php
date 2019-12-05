93495120d914ca5f0b3fa77b0e070b97f58d7213
30kajal <301999kajal@gmail.com>
2019-12-05 06:22:04 +0530

    fourth commit
    more updated web work

--- ks/med/carview.php
+++ ks/med/carview.php
@@ -2,18 +2,17 @@
 $m=$_POST['name'];
 
 $con=mysqli_connect('localhost','root');
-mysqli_select_db($con,'db5');
-$q="select *from db5 ";
+mysqli_select_db($con,'db6');
+$q="select *from db6 ";
 $result=mysqli_query($con,$q);
 $num=mysqli_num_rows($result);
-for($i=1;$i<=$num;$i++)
-{
+for($i=1;$i<=$num;$i++){
     $row=mysqli_fetch_array($result);
     $s=$row['med'];
     
    if($m==$s)
 {  echo"<script>location.href='welcome.html'</script>";
-    
+    exit();
 }
 }
 echo"<script>location.href='wrong.html'</script>";
--- ks/med/menview.php
+++ ks/med/menview.php
@@ -2,18 +2,17 @@
 $m=$_POST['name'];
 
 $con=mysqli_connect('localhost','root');
-mysqli_select_db($con,'db5');
-$q="select *from db5 ";
+mysqli_select_db($con,'db9');
+$q="select *from db9 ";
 $result=mysqli_query($con,$q);
 $num=mysqli_num_rows($result);
-for($i=1;$i<=$num;$i++)
-{
+for($i=1;$i<=$num;$i++){
     $row=mysqli_fetch_array($result);
     $s=$row['med'];
     
    if($m==$s)
 {  echo"<script>location.href='welcome.html'</script>";
-    
+    exit();
 }
 }
 echo"<script>location.href='wrong.html'</script>";
